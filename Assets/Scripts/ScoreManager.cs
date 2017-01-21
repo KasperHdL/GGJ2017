c9f583ca5938d6fd4e40ea95073dc23e0db0863d
Kasper Honnens de Lichtenberg <kasper@honnens.dk>
2017-01-22 00:38:24 +0100

    Created scoreboard

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,19 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour {
-
-
+    
     public int score = 0;
+    public Text scoreText;
 
     private static ScoreManager instance;
 
     public void Awake(){
         if(instance != null)
-            throw new UnityException("Muliple score manager found");
+            throw new UnityException("Multiple score manager found");
 
         instance = this;
+    }
+	
+    void Update() {
+        scoreText.text = score.ToString();
     }
 
     public static ScoreManager getInstance(){
