1a7b1fd53eb37c81632e0690f89c749c006c682a
Jonas VHM <jonasvhm@gmail.com>
2017-01-21 01:14:03 +0100

    The ovendoors now work, but without gravity, and possibly without hints

---
+++ Assets/Scripts/OvenDoor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OvenDoor : MonoBehaviour {
+
+	public float forceAmount;
+	public Vector3 theForce;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		theForce = new Vector3 (0, forceAmount, 0);
+		this.transform.gameObject.GetComponent<Rigidbody> ().AddTorque (theForce);
+	}
+}
