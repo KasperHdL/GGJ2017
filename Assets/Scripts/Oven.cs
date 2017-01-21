5b616f5b906bfbf9375c9d738b7fc228edb6922a
Jonas VHM <jonasvhm@gmail.com>
2017-01-21 14:39:20 +0100

    Hopefully made the oven call a closed when the door is closed
    Also made a script that can cook ingredients, but not pizza

---
+++ Assets/Scripts/Oven.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oven : MonoBehaviour {
+
+	public bool closed = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void isClosed(bool closed){
+		this.closed = closed;
+	}
+
+	void OnTriggerStay(Collider other) {
+		if (closed) {
+			if (other.tag.Equals ("Pizza")) {
+				other.GetComponent<Pizza> ().cook ();
+			}
+		}
+	}
+}
