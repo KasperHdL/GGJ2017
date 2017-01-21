7d37908c5274643f2d7d823da3e3106dd479e344
Kasper Honnens de Lichtenberg <khon@itu.dk>
2017-01-21 13:58:56 +0100

    Ingredient dispensers work

--- Assets/IngredientDispenser.cs
+++ Assets/IngredientDispenser.cs
@@ -1,17 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class IngredientDispenser : MonoBehaviour {
-    public GameObject prefab;
+namespace Valve.VR.InteractionSystem
+{
+    [RequireComponent(typeof(Interactable))]
+    public class IngredientDispenser : MonoBehaviour {
+        public GameObject prefab;
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        private Hand interactingHand;
+        private bool canDispense = true;
+
+        private void OnHandHoverBegin(Hand hand)
+        {
+            if (interactingHand == null)
+            {
+                interactingHand = hand;
+            }
+        }
+
+        private void HandHoverUpdate(Hand hand)
+        {
+            //Trigger got pressed
+            if (hand.GetStandardInteractionButtonDown() && canDispense)
+            {
+                canDispense = false;
+                Transform attachPoint = hand.transform;
+                Instantiate(prefab, attachPoint.position, Quaternion.identity);
+            }
+        }
+
+        private void OnHandHoverEnd(Hand hand)
+        {
+            if (interactingHand == hand)
+            {
+                canDispense = true;
+            }
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+
+        void OnTriggerStay(Collider other)
+        {
+            if (other.tag == "GameController")
+            {
+
+            }
+        }
+    }
 }
