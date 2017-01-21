ad8ebc6f04451a4f6dac0fcdff3dbc1da8c38777
Jonas VHM <jonasvhm@gmail.com>
2017-01-21 19:28:36 +0100

    Made oven ready for sounds

--- Assets/Scripts/Oven.cs
+++ Assets/Scripts/Oven.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class Oven : MonoBehaviour {
 
 	public List<GameObject> pizzasInOven;
+	private AudioSource audioSrc;
+	public AudioClip[] bakeSounds;
 	// Use this for initialization
 	void Start () {
-		
+		audioSrc = GetComponent<AudioSource> ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
 	public void doorClosed(){
+		int index = Random.Range (0, bakeSounds.Length + 1);
+		audioSrc.clip = bakeSounds [index];
+		if (!audioSrc.isPlaying) {
+			audioSrc.Play ();
+		}
 		foreach (GameObject pizza in pizzasInOven) {
 			pizza.GetComponent<Pizza> ().cook ();
 		}
