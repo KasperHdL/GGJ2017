d8cf285fb2816b01cd5081da5d37da0c8002235b
Jonas VHM <jonasvhm@gmail.com>
2017-01-21 23:44:19 +0100

    Fixed merge issues, and made bell sounds

--- Assets/Scripts/Bell.cs
+++ Assets/Scripts/Bell.cs
@@ -7,8 +7,13 @@ public class Bell : MonoBehaviour {
 
     [HideInInspector] public GameManager gameManager;
 
+	private AudioSource audioSrc;
+	//Remember to put ind sounds if you need them
+	public AudioClip[] bellSounds;
+
     public void Start(){
         gameManager = GameManager.getInstance();
+		audioSrc = GetComponent<AudioSource>();
     }
 
     private void HandHoverUpdate(Valve.VR.InteractionSystem.Hand hand)
@@ -16,6 +21,13 @@ public class Bell : MonoBehaviour {
         if (hand.GetStandardInteractionButtonDown())
         {
             gameManager.startGame();
+			if (bellSounds.Length > 0) {
+				int index = Random.Range (0, bellSounds.Length);
+				audioSrc.clip = bellSounds [index];
+				if (!audioSrc.isPlaying) {
+					audioSrc.Play ();
+				}
+			}
 
         }
     }
