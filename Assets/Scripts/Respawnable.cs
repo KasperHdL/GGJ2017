4664b9535d3936be902fec8de3485295ac3b60b1
Unknown <mdhe@itu.dk>
2017-09-25 22:14:52 +0200

    Fixed typo in credits, made clipboard respawn if on floor

--- Assets/Scripts/Respawnable.cs
+++ Assets/Scripts/Respawnable.cs
@@ -3,14 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Respawnable : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
+	public float timeBeforeRespawn;
+	private Vector3 originPosition;
+	private Quaternion originRotation;
+	private float floorTimer;
+	private bool onFloor;
+	private void Start() 
+	{
+		originPosition = transform.position;
+		originRotation = transform.rotation;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	private void Update() 
+	{
+		if (!onFloor) 
+		{
+			floorTimer = 0;
+			return;
+		}
+		else 
+		{
+			floorTimer += Time.deltaTime;
+		}
+		if (floorTimer > timeBeforeRespawn) 
+		{
+			transform.position = originPosition;
+			transform.rotation = originRotation;
+		}
+	}
+
+	private void OnCollisionEnter(Collision other) 
+	{
+		if (other.gameObject.tag == "Floor") 
+		{
+			onFloor = true;
+		}
+	}
+
+	private void OnCollisionExit(Collision other) 
+	{
+		if (other.gameObject.tag == "Floor") 
+		{
+			onFloor = false;
+		}
 	}
 }
